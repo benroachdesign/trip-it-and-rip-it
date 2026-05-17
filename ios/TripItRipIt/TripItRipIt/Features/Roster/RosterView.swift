3afa0e31ec8a5a34ce3503e9ed7820c72fc57e34
Ben Roach <benroach.design@gmail.com>
2026-05-17 16:20:17 -0500

    Fix cities: Strub is Madison, Derek is Minneapolis

--- ios/TripItRipIt/TripItRipIt/Features/Roster/RosterView.swift
+++ ios/TripItRipIt/TripItRipIt/Features/Roster/RosterView.swift
@@ -91,13 +91,13 @@ extension Member {
 extension Member {
     static let allMockMembers: [Member] = [
         Member(id: UUID(), fullName: "Ben Roach",       nickname: "Roach",  sortOrder: 10,  isGuest: false, isOg: true,  homeCity: "Chicago",       handicap: 11, funFact: nil, bio: nil),
-        Member(id: UUID(), fullName: "Ryan Strub",      nickname: "Strub",  sortOrder: 20,  isGuest: false, isOg: true,  homeCity: "Portland",      handicap: 12, funFact: nil, bio: nil),
+        Member(id: UUID(), fullName: "Ryan Strub",      nickname: "Strub",  sortOrder: 20,  isGuest: false, isOg: true,  homeCity: "Madison",       handicap: 12, funFact: nil, bio: nil),
         Member(id: UUID(), fullName: "Austin Mader",    nickname: "Mader",  sortOrder: 30,  isGuest: false, isOg: true,  homeCity: "Dallas",        handicap: 18, funFact: nil, bio: nil),
         Member(id: UUID(), fullName: "Braden Carlson",  nickname: "Braden", sortOrder: 40,  isGuest: false, isOg: true,  homeCity: "Chicago",       handicap: 10, funFact: nil, bio: nil),
         Member(id: UUID(), fullName: "Matt Webb",       nickname: "Webb",   sortOrder: 50,  isGuest: false, isOg: false, homeCity: "Austin",        handicap: 9,  funFact: nil, bio: nil),
         Member(id: UUID(), fullName: "Tommer Butman",   nickname: "Tommer", sortOrder: 60,  isGuest: false, isOg: false, homeCity: "Chicago",       handicap: 12, funFact: nil, bio: nil),
         Member(id: UUID(), fullName: "Chris Lutz",      nickname: "Lutz",   sortOrder: 70,  isGuest: false, isOg: false, homeCity: "San Francisco", handicap: 13, funFact: nil, bio: nil),
-        Member(id: UUID(), fullName: "Derek DeCarolis", nickname: "Derek",  sortOrder: 80,  isGuest: false, isOg: false, homeCity: "Houston",       handicap: 13, funFact: nil, bio: nil),
+        Member(id: UUID(), fullName: "Derek DeCarolis", nickname: "Derek",  sortOrder: 80,  isGuest: false, isOg: false, homeCity: "Minneapolis",   handicap: 13, funFact: nil, bio: nil),
         Member(id: UUID(), fullName: "Alex Blizniak",   nickname: "Bliz",   sortOrder: 90,  isGuest: true,  isOg: false, homeCity: "Chicago",  handicap: 11, funFact: nil, bio: nil),
         Member(id: UUID(), fullName: "Kyle Worley",     nickname: "Kyle",   sortOrder: 100, isGuest: true,  isOg: false, homeCity: "Portland", handicap: 4,  funFact: nil, bio: nil),
         Member(id: UUID(), fullName: "Mike Steward",    nickname: "Mike",   sortOrder: 110, isGuest: true,  isOg: false, homeCity: "Austin",   handicap: 7,  funFact: nil, bio: nil)
---
+++ supabase/migrations/20260517000014_fix_strub_derek_cities.sql
@@ -0,0 +1,2 @@
+update members set home_city = 'Madison'     where nickname = 'Strub';
+update members set home_city = 'Minneapolis' where nickname = 'Derek';
