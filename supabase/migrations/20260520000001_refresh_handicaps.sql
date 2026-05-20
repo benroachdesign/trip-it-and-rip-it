0282ebb377f853c649efa3dab42cc1dd8c67bac4
Ben Roach <benroach.design@gmail.com>
2026-05-20 10:19:17 -0500

    Refresh handicaps for the 8 named core members (now decimals)

---
+++ supabase/migrations/20260520000001_refresh_handicaps.sql
@@ -0,0 +1,8 @@
+update members set handicap = 11.6 where nickname = 'Roach';
+update members set handicap = 15.0 where nickname = 'Strub';
+update members set handicap = 17.0 where nickname = 'Mader';
+update members set handicap = 12.0 where nickname = 'Braden';
+update members set handicap = 9.2  where nickname = 'Webb';
+update members set handicap = 14.8 where nickname = 'Tommer';
+update members set handicap = 11.3 where nickname = 'Lutz';
+update members set handicap = 12.6 where nickname = 'Derek';
